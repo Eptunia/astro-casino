fe25f9a4484f1aa5c05bae0cbbd5923d2b176065
Eptunia <soschorizo@gmail.com>
2026-09-19 20:40:43 +0200

    Ajout du deploiement Firebase et nettoyage

---
+++ firebase.js
@@ -0,0 +1,17 @@
+{
+  "hosting": {
+    "public": ".",
+    "ignore": [
+      "firebase.json",
+      "firestore.rules",
+      "*.md",
+      "*.zip",
+      "package*.json",
+      "**/.*",
+      "**/node_modules/**"
+    ]
+  },
+  "firestore": {
+    "rules": "firestore.rules"
+  }
+}
